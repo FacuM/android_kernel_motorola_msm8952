3800735b6182e986ff47f8fe334ebf83254ee23d
FacuM <facumo.fm@gmail.com>
2017-07-05 03:58:49 +0000

    base: topology: fix some merge mistakes.

--- drivers/base/topology.c
+++ drivers/base/topology.c
@@ -29,27 +29,22 @@
 #include <linux/module.h>
 #include <linux/hardirq.h>
 #include <linux/topology.h>
-
 #define define_one_ro_named(_name, _func)				\
 	static DEVICE_ATTR(_name, 0444, _func, NULL)
-
 #define define_one_ro(_name)				\
 	static DEVICE_ATTR(_name, 0444, show_##_name, NULL)
-
 #define define_id_show_func(name)				\
 static ssize_t show_##name(struct device *dev,			\
 		struct device_attribute *attr, char *buf)	\
 {								\
 	return sprintf(buf, "%d\n", topology_##name(dev->id));	\
 }
-
 #if defined(topology_thread_cpumask) || defined(topology_core_cpumask) || \
     defined(topology_book_cpumask)
 static ssize_t show_cpumap(int type, const struct cpumask *mask, char *buf)
 {
 	ptrdiff_t len = PTR_ALIGN(buf + PAGE_SIZE - 1, PAGE_SIZE) - buf;
 	int n = 0;
-
 	if (len > 1) {
 		n = type?
 			cpulist_scnprintf(buf, len-2, mask) :
@@ -60,7 +55,6 @@ static ssize_t show_cpumap(int type, const struct cpumask *mask, char *buf)
 	return n;
 }
 #endif
-
 #ifdef arch_provides_topology_pointers
 #define define_siblings_show_map(name)					\
 static ssize_t show_##name(struct device *dev,				\
@@ -69,7 +63,6 @@ static ssize_t show_##name(struct device *dev,				\
 	unsigned int cpu = dev->id;					\
 	return show_cpumap(0, topology_##name(cpu), buf);		\
 }
-
 #define define_siblings_show_list(name)					\
 static ssize_t show_##name##_list(struct device *dev,			\
 				  struct device_attribute *attr,	\
@@ -78,7 +71,6 @@ static ssize_t show_##name##_list(struct device *dev,			\
 	unsigned int cpu = dev->id;					\
 	return show_cpumap(1, topology_##name(cpu), buf);		\
 }
-
 #else
 #define define_siblings_show_map(name)					\
 static ssize_t show_##name(struct device *dev,				\
@@ -86,7 +78,6 @@ static ssize_t show_##name(struct device *dev,				\
 {									\
 	return show_cpumap(0, topology_##name(dev->id), buf);		\
 }
-
 #define define_siblings_show_list(name)					\
 static ssize_t show_##name##_list(struct device *dev,			\
 				  struct device_attribute *attr,	\
@@ -95,24 +86,18 @@ static ssize_t show_##name##_list(struct device *dev,			\
 	return show_cpumap(1, topology_##name(dev->id), buf);		\
 }
 #endif
-
 #define define_siblings_show_func(name)		\
 	define_siblings_show_map(name); define_siblings_show_list(name)
-
 define_id_show_func(physical_package_id);
 define_one_ro(physical_package_id);
-
 define_id_show_func(core_id);
 define_one_ro(core_id);
-
 define_siblings_show_func(thread_cpumask);
 define_one_ro_named(thread_siblings, show_thread_cpumask);
 define_one_ro_named(thread_siblings_list, show_thread_cpumask_list);
-
 define_siblings_show_func(core_cpumask);
 define_one_ro_named(core_siblings, show_core_cpumask);
 define_one_ro_named(core_siblings_list, show_core_cpumask_list);
-
 #ifdef CONFIG_SCHED_BOOK
 define_id_show_func(book_id);
 define_one_ro(book_id);
@@ -120,7 +105,6 @@ define_siblings_show_func(book_cpumask);
 define_one_ro_named(book_siblings, show_book_cpumask);
 define_one_ro_named(book_siblings_list, show_book_cpumask_list);
 #endif
-
 static struct attribute *default_attrs[] = {
 	&dev_attr_physical_package_id.attr,
 	&dev_attr_core_id.attr,
@@ -135,33 +119,26 @@ static struct attribute *default_attrs[] = {
 #endif
 	NULL
 };
-
 static struct attribute_group topology_attr_group = {
 	.attrs = default_attrs,
 	.name = "topology"
 };
-
 /* Add/Remove cpu_topology interface for CPU device */
 static int __cpuinit topology_add_dev(unsigned int cpu)
 {
 	struct device *dev = get_cpu_device(cpu);
-
 	return sysfs_create_group(&dev->kobj, &topology_attr_group);
 }
-
 static void __cpuinit topology_remove_dev(unsigned int cpu)
 {
 	struct device *dev = get_cpu_device(cpu);
-
 	sysfs_remove_group(&dev->kobj, &topology_attr_group);
 }
-
 static int __cpuinit topology_cpu_callback(struct notifier_block *nfb,
 					   unsigned long action, void *hcpu)
 {
 	unsigned int cpu = (unsigned long)hcpu;
 	int rc = 0;
-
 	switch (action) {
 	case CPU_UP_PREPARE:
 	case CPU_UP_PREPARE_FROZEN:
@@ -176,24 +153,16 @@ static int __cpuinit topology_cpu_callback(struct notifier_block *nfb,
 	}
 	return notifier_from_errno(rc);
 }
-
 static int __cpuinit topology_sysfs_init(void)
 {
 	int cpu;
-	int rc = 0;
-
-	cpu_notifier_register_begin();
-
+	int rc;
 	for_each_online_cpu(cpu) {
 		rc = topology_add_dev(cpu);
 		if (rc)
-			goto out;
+			return rc;
 	}
-	__hotcpu_notifier(topology_cpu_callback, 0);
-
-out:
-	cpu_notifier_register_done();
-	return rc;
+	hotcpu_notifier(topology_cpu_callback, 0);
+	return 0;
 }
-
 device_initcall(topology_sysfs_init);
